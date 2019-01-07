3d68f4b14ea6fa257e5f5bd30d868d607c17016b
Ksenia <ksenyanya74@gmail.com>
2019-01-07 19:40:49 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -223,11 +223,3 @@ function newQuestion(msg){
         full_answer: ' В данном слове ударение должно быть поставлено на слог с буквой О — дОнизу.'
       }
     ];
-
-
-
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-app.listen(port);
